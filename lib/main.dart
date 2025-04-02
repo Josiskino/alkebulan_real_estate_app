2fda056f0c233e29dffb38038b6562681ab64f7c
Josiskino <lasselus15@gmail.com>
2025-04-02 19:14:39 +0000

    adding starter screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'presentation/views/home_screen.dart';
-
+import 'presentation/views/starter_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -17,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const StarterScreen(),
     );
   }
 }
